0fd10773eb1aaadea3b6f6d26ec62989189d666f
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-09 13:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import openai
 app = Flask(__name__)
 
 # Set your OpenAI API key here
-openai.api_key = "YOUR_OPENAI_API_KEY"
+openai.api_key = "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"
 
 @app.route('/get_answer', methods=['POST'])
 def get_answer():
