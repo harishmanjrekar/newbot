5d880f920c0678eedab7249dc9c36ced3e643415
harishmanjrekar <54299326+harishmanjrekar@users.noreply.github.com>
2023-08-09 13:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import openai
 app = Flask(__name__)
 
 # Set your OpenAI API key here
-openai.api_key = "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"
+openai.api_key = "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""
 
 @app.route('/get_answer', methods=['POST'])
 def get_answer():
     user_input = request.json['user_input']
     
     response = openai.Completion.create(
-        engine="curie",
+        engine="davinci",  # Use the appropriate engine
         prompt=user_input,
-        max_tokens=200  # Adjust as needed
+        max_tokens=50  # Adjust as needed
     )
     
     answer = response.choices[0].text.strip()
